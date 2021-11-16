f308693b360b1832c3fb961ee5beee969da8feda
Shomo3 <Shomoa.BSC@gmail.com>
2021-11-16 10:08:46 +0200

    16-11

--- SPOS/AppDelegate.swift
+++ SPOS/AppDelegate.swift
@@ -6,17 +6,104 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
+import SVProgressHUD
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
+    var window: UIWindow?
+    var hasAlreadyLaunched: Bool!
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        IQKeyboardManager.shared.enable = true
+
+        UIFont.overrideInitialize()
+        removeBottomLineFromNavigation()
+        setFontTypeUINavigationController()
+        removeTabBarTopLine()
+        setImageForBackArrow()
+        customSVProgressHUD()
+        
+        checkFirstTimeInstallApp()
+        
         return true
     }
-
+    
+    func checkFirstTimeInstallApp() {
+        //retrieve value from local store, if value doesn't exist then false is returned
+        hasAlreadyLaunched = UserDefaults.standard.bool(forKey: "hasAlreadyLaunched")
+        //check first launched
+        if (hasAlreadyLaunched){
+            hasAlreadyLaunched = true
+        }else{
+            UserDefaults.standard.set(true, forKey: "hasAlreadyLaunched")
+        }
+    }
+    
+    func sethasAlreadyLaunched(){
+        hasAlreadyLaunched = true
+    }
+    
+    
+    func customSVProgressHUD()  {
+        SVProgressHUD.setBackgroundColor(.clear)
+        SVProgressHUD.setForegroundColor("0386E8".color)
+        SVProgressHUD.setDefaultMaskType(.black)
+    }
+    
+    func removeBottomLineFromNavigation() {
+        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .any, barMetrics: .default)
+        UINavigationBar.appearance().shadowImage = UIImage()
+    }
+    
+    func setFontTypeUINavigationController() {
+        
+        let attrs = [
+            NSAttributedString.Key.foregroundColor: "202124".color,
+            NSAttributedString.Key.font: UIFont(name: "JFFlat-Regular", size: 12)!
+        ]
+        
+        UINavigationBar.appearance().titleTextAttributes = attrs
+    }
+    
+    func removeTabBarTopLine() {
+        UITabBar.appearance().shadowImage = UIImage()
+        UITabBar.appearance().backgroundImage = UIImage()
+    }
+    
+    func setImageForBackArrow() {
+        
+        let insets = UIEdgeInsets(top: 0, left: 0, bottom: 2, right: 0)
+        let backArrowImage = UIImage(named: "img-arrow-English")?.withAlignmentRectInsets(insets)
+        UINavigationBar.appearance().backIndicatorImage = backArrowImage
+        UINavigationBar.appearance().backIndicatorTransitionMaskImage = backArrowImage
+        
+    }
+    
+    
+    
+    func presentLoginViewController(animated:Bool) {
+        
+        let controller = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "startNav")
+        
+        if animated {
+            let options: UIView.AnimationOptions = [.transitionCrossDissolve, .curveEaseOut]
+            
+            UIView.transition(with: self.window!, duration: 0.4, options: options, animations: {
+                self.window?.rootViewController = controller
+            }, completion: nil)
+            
+        } else {
+            self.window?.rootViewController = controller
+        }
+    }
+    
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
@@ -32,5 +119,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+    
+   
+    
 }
 
